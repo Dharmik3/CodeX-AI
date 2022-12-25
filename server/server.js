3ab779a4401180c89947baca13adeb7a04aa1247
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2022-12-25 11:39:46 +0530

    openAi server

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { Configuration, OpenAIApi } = require("openai");
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey:process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration);
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message:'From OpenAI CodeX'
+    })
+})
+
+app.post('/', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`,
+            temperature: 0,
+            max_tokens: 3000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({error})
+    }
+})
+
+app.listen(5005, () => {
+    console.log(`server is running on port http://localhost:5005`);
+})
