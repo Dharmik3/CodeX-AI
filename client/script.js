480f4f2d86e7de02602bfbdc95e86709d4cf0faf
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2022-12-25 18:02:01 +0530

    server url updated

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
 
   // coonecting to the server
-  const response = await fetch('https://codex-ai-v9q6.onrender.com', {
+  const response = await fetch('https://codex-ai-v9q6.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
